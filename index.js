dd5b023a3932c7d423e38fd4febf8c3e4ebe9e51
larswolff <lw@synpro-digital.de>
2012-11-04 22:34:19 +0100

    refactored module and index

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-var s = require('./lib/suhail-publisher')
-  , fs = require('fs')
-  , config = JSON.parse(fs.readFileSync("./config.json"));
+var fs = require('fs');
+var util = require('util');
+var config = JSON.parse(fs.readFileSync("./config.json"));
+var parserName = config.parser;
 
-suhailPublisher = s.init(config);
-console.log(suhailPublisher);
-//suhailPublisher.watch();
+//console.log(parserName);
+//console.log('./lib/parsers/' + parserName + '.js');
+//console.log(fs.existsSync('./lib/parsers/' + parserName + '.js'));
+
+//fs.existsSync('./lib/parsers/' + parserName + '.js') || process.exit(1);
+fs.existsSync('./lib/parsers/' + parserName + '.js') || (function () { throw new Error('no such parser') }());
+
+var SuhailParser = require('./lib/parsers/' + parserName).create();
+var SuhailPublisher = require('./lib/suhail-publisher').create(config);
+
+console.log(SuhailParser);
+
+SuhailPublisher.watch();
+
+console.log(util.inspect(SuhailPublisher, true, 100));
+
+process.on('exit', function () {
+	SuhailPublisher.unwatch();
+});
+
+process.on('uncaughtException', function (err) {
+  console.log('Caught exception: ' + err);
+});
