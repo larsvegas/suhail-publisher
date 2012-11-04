d2c589279a1b1bbee3eb5e54e1bfd95115c46750
larswolff <lw@synpro-digital.de>
2012-11-04 16:41:20 +0100

    mproved config, refactored code

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-var SuhailPublisher = require('./lib/suhail-publisher')
+var s = require('./lib/suhail-publisher')
   , fs = require('fs')
   , config = JSON.parse(fs.readFileSync("./config.json"));
 
-suhailPublisher = SuhailPublisher.init(config);
-suhailPublisher.watch();
+suhailPublisher = s.init(config);
+console.log(suhailPublisher);
+//suhailPublisher.watch();
