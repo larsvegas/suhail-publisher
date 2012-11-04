26493a711d7ec2ad685b902bd26c9fdc3159f3b1
larswolff <lw@synpro-digital.de>
2012-11-04 23:54:34 +0100

    refactored index

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 var fs = require('fs');
 var util = require('util');
 var config = JSON.parse(fs.readFileSync("./config.json"));
-var parserName = config.parser;
-
-//console.log(parserName);
-//console.log('./lib/parsers/' + parserName + '.js');
-//console.log(fs.existsSync('./lib/parsers/' + parserName + '.js'));
-
-//fs.existsSync('./lib/parsers/' + parserName + '.js') || process.exit(1);
-fs.existsSync('./lib/parsers/' + parserName + '.js') || (function () { throw new Error('no such parser') }());
-
-var SuhailParser = require('./lib/parsers/' + parserName).create();
 var SuhailPublisher = require('./lib/suhail-publisher').create(config);
+//console.log(util.inspect(SuhailPublisher, true, 100));
 
-console.log(SuhailParser);
+// configure parser
+// TODO: this shoud be an abstract factory
+var parserName = config.parser;
+fs.existsSync('./lib/parsers/' + parserName + '.js') || (function () { throw new Error('no such parser') }());
+var SuhailParser = require('./lib/parsers/' + parserName).create();
+//console.log(util.inspect(SuhailParser, true, 100));
 
-SuhailPublisher.watch();
 
-console.log(util.inspect(SuhailPublisher, true, 100));
+SuhailPublisher.watch(function parseContent(content) {
+	SuhailParser.parseContent(content, function publishParsedConten(data) {
+		SuhailPublisher.publish(data);
+	})
+});
 
+// termination
 process.on('exit', function () {
 	SuhailPublisher.unwatch();
 });
