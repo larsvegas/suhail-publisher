63a04b6c54bea4e296d7009661d65049a5b72d6b
larsvegas <larsvegas@synpro.io>
2013-11-17 14:51:20 +0100

    jshint all the things

--- suhail-publisher.js
+++ suhail-publisher.js
@@ -1,28 +1,36 @@
 "use strict";
 
-process.title = "suhail: publisher";
+process.title = 'suhail: publisher';
 
 var fs = require('fs');
-fs.existsSync('./config.json') || (function () { throw new Error('config.json does not exists') }());
-var config = JSON.parse(fs.readFileSync("./config.json"));
+
+// does config exist?
+if (!fs.existsSync('./config.json')) {
+  throw new Error('config.json does not exists');
+}
+
+var config = JSON.parse(fs.readFileSync('./config.json'));
 var SuhailPublisher = require('./lib/suhail-publisher').create(config);
 
-// configure parser
-// TODO: this shoud be an abstract factory
+// configure parser, and check if it exist
+// TODO: Factorize!
 var parserName = config.parser;
-fs.existsSync('./lib/parsers/' + parserName + '.js') || (function () { throw new Error('no such parser') }());
+if (!fs.existsSync('./lib/parsers/' + parserName + '.js')) {
+  throw new Error('no such parser');
+}
+
 var SuhailParser = require('./lib/parsers/' + parserName).create();
 
 SuhailPublisher.watch(function parseContent(content) {
   var options = {
     suhailStationId : config.suhailStationId,
-  }
+  };
 
   SuhailParser.parseContent(content, options, function publishParsedConten(data) {
     data = JSON.stringify(data);
     data = JSON.parse(data);
     SuhailPublisher.publish(data);
-  })
+  });
 });
 
 // termination
@@ -33,3 +41,4 @@ process.on('exit', function () {
 process.on('uncaughtException', function (err) {
   console.log('Caught exception: ' + err);
 });
+
